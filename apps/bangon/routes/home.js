cf755a4b255bd046f9bf2323e0f4ef364e4a8ae2
Craig Cook <craig_a_cook@hotmail.com>
2013-12-04 14:06:25 +0800

    add post to login/callback

--- apps/bangon/routes/home.js
+++ apps/bangon/routes/home.js
@@ -184,6 +184,7 @@ exports.loginCallback = function (req, res, next) {
       // if publish_actions permission is missing - go to login dialog
       if (!result.data.publish_actions || result.data.publish_actions == 0) {
         console.log("publish_actions: " + result.data.publish_actions);
+        req.session = null; // clear session
         return res.redirect('/');
       }
 
