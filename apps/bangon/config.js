367154cee4d492ea35984cb8df3a8e81f3d99702
Craig Cook <craig_a_cook@hotmail.com>
2013-12-04 10:02:16 +0800

    add post to login/callback

--- apps/bangon/config.js
+++ apps/bangon/config.js
@@ -8,7 +8,7 @@ config.facebook = {
     appId:          process.env.FACEBOOK_APP_ID         || '719182394777999',
     appSecret:      process.env.FACEBOOK_SECRET         || '16d4c00cb0bfdaa335b043d333a40f08',
     appNamespace:   process.env.FACEBOOK_APPNAMESPACE   || 'bang-on',
-    redirectUri:    process.env.FACEBOOK_REDIRECTURI    ||  config.rootUrl + 'login/callback',
+    redirectUri:    process.env.FACEBOOK_REDIRECTURI    ||  '/' + 'login/callback',
     
     scope: 'email,user_about_me,user_birthday,user_education_history,user_location,publish_actions,user_likes'
 };
