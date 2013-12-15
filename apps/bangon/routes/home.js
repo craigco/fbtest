830932c1f18e2071b12cfcd42f15c0a6baf366fc
craig <craig_a_cook@hotmail.com>
2013-12-15 22:37:11 +0800

    set up data for inserting into profile document

--- apps/bangon/routes/home.js
+++ apps/bangon/routes/home.js
@@ -112,9 +112,7 @@ exports.indexPost = function (req, res) {
           console.log(error);
           throw(error);
         } else {
-          console.log(document);
-          console.log(document.profile);
-          if (result.profile == null) {
+          if (document.profile == null) {
             // no profile
             console.log("redirecting " + userInfo.id + " to get profile information");
 
